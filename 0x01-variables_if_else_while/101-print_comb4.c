d5bfe7709cdf39365cd51155c0d00c29caf92851
ophiliatex <ophiliatex@gmail.com>
2023-04-21 20:17:40 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible different combinations of three digits
+* Return: 0
+*/
+int main(void)
+{
+int d, t, f;
+
+for (d = 0; d < 10; d++)
+{
+for (t = d + 1; t < 10; t++)
+{
+for (f = t + 1; f < 10; f++)
+{
+putchar(d + '0');
+putchar(t + '0');
+putchar(f + '0');
+if (d != 5 || t != 6 || f != 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
