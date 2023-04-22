00777109f53f4a0efcf81d3d0927f117314a27ee
ophiliatex <ophiliatex@gmail.com>
2023-04-21 19:41:45 -0700

    comb too

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int main(void)
 
 	int num;
 
-	for (num = 33; num < 65; num++)
+	for (num = 0; num < 10; num++)
 
 	{
 
-		putchar(num);
+		putchar(num + '0');
 
-	if (num != 63)
+	if (num < 9)
 
 	{
 
