726a15453cdd71c6052082e54fef7841c8e22955
ophiliatex <ophiliatex@gmail.com>
2023-04-21 18:24:39 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point prints the alphabet in lowercase,
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char ap;
+
+	for (ap = 'a'; ap <= 'z'; ap++)
+
+	{
+		putchar(ap);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
