ff9d9420f1b20c3ad6df0ae6e4552322d39ee14d
ophiliatex <ophiliatex@gmail.com>
2023-04-25 20:28:06 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs -Entry computes the absolute value of an integer
+ * @n: the value to be  checked
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
