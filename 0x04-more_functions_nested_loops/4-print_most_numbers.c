2a0b25a65edc0b569e8b8e23705d55103fac787b
ophiliatex <ophiliatex@gmail.com>
2023-04-27 19:27:01 -0700

    Most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,14 @@
  * Return: void .
  */
 
-int void print_most_numbers(void)
+void print_most_numbers(void)
 {
 int i;
+
 for (i = 0; i <= 9; i++)
 {
-	if (i != 2 && i != 4)
-		_putchar(i + '0');
+if (i != 2 && i != 4)
+_putchar(i + '0');
 }
-_putchar("\n");
+_putchar('\n');
 }
