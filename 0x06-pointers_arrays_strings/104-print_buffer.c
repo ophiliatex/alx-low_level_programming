c80bd83b08d59d09def3ffdfc8d8fb2134b27e03
ophiliatex <ophiliatex@gmail.com>
2023-05-05 18:53:30 -0700

    check

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,51 +1,81 @@
 #include <stdio.h>
 #include "main.h"
 
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isPrintableASCII - determines if n is a printable ASCII char
+ * @n: integer
+ * Return: 1 if true, 0 if false
+ */
+int isPrintableASCII(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+
+/**
+ * printHexes - print hex values for string b in formatted form
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printHexes(char *b, int start, int end)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+}
+
+/**
+ * printASCII - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
+
 /**
  * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of the buffer
+ * @b: string
+ * @size: size of buffer
  */
 void print_buffer(char *b, int size)
 {
-int i, a, c, d = 0;
+	int start, end;
 
-if (size <= 0)
-printf("\n");
-else
-{
-while (i < size)
-{
-printf("%08x: ", i);
-a = 0;
-while (a < 10)
-{
-if (i + a < size)
-printf("%02x", b[i + a]);
-else
-printf("  ");
-if (a % 2 != 0)
-printf(" ");
-a++;
-}
-printf("; ");
-c = 0;
-while (c < 10)
-{
-if (i + c < size)
-{
-d = b[i + c];
-if (d >= 32 && d <= 126)
-printf("%c", d);
-else
-printf(".");
-}
-else
-printf(" ");
-c++;
-}
-printf("\n");
-i += 10;
-}
-}
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
+		}
+	} else
+		printf("\n");
 }
