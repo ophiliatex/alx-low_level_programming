1f1fd7cb8ff9a4b55c1b1c7f4f0178f3479bda6c
ophiliatex <ophiliatex@gmail.com>
2023-05-05 17:00:51 -0700

    Numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number -Prints an integer
+ * @n: integer to print
+ * Return: void
+ */
+void print_number(int n)
+{
+unsigned int num;
+
+if (n < 0)
+{
+_putchar('-');
+num = -n;
+}
+else
+{
+num = n;
+}
+
+if (num / 10)
+print_number(num / 10);
+
+_putchar((num % 10) + '0');
+}
