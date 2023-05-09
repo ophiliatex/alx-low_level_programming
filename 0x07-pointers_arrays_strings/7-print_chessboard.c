1aa8ff40bc7e4979936fd2001245afe9ae1b79a7
ophiliatex <ophiliatex@gmail.com>
2023-05-08 17:54:08 -0700

    chess not checker

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard -A function that prints the chessboard.
+ * @a: An array
+ * Return: 0 (success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int index, num;
+
+	for (index = 0; index < 8; index++)
+	{
+		for (num = 0; num < 8; num++)
+		{
+			_putchar(a[index][num]);
+			_putchar('\n');
+		}
+	}
+}
