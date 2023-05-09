1525388222ed29a96476cd11ee54043ff51dfdb0
ophiliatex <ophiliatex@gmail.com>
2023-05-08 18:12:09 -0700

    chess not checker

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,9 +14,7 @@ void print_chessboard(char (*a)[8])
 	for (index = 0; index < 8; index++)
 	{
 		for (num = 0; num < 8; num++)
-		{
 			_putchar(a[index][num]);
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
