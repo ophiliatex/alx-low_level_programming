a1f8a4acbcac5c28b8da4693206e47fa1cba851a
ophiliatex <ophiliatex@gmail.com>
2023-05-10 20:12:18 -0700

    POW

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  * _pow_recursion - Returns the value of x raised to the power of y.
  * @x: Base value
  * @y: the exponent
- * Retrun: -1 if y is lower than 0
+ * Return: -1 if y is lower than 0
  */
 
 int _pow_recursion(int x, int y)
