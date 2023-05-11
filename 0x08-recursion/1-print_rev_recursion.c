818a21d55e7465e7f09977943321794a8656124b
ophiliatex <ophiliatex@gmail.com>
2023-05-10 19:31:38 -0700

    reverses

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string in reverse, followed by a new line.
+ * _print_rev_recursion - prints a string in reverse, followed by a new line.
  * @s: string to be printed
  * Retrun: void.
  */
@@ -10,7 +10,6 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
 
