413b256a4733e07823539b9bdc7445c2ebc9f57b
ophiliatex <ophiliatex@gmail.com>
2023-05-21 16:06:13 -0700

    argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints a number of argument passed to it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: void
+ */
+
+int main (int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc -1);
+	return (0);
+}
