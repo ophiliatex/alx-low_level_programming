78fb174ffb136c29f5df5b50c2bf4744c6b70776
ophiliatex <ophiliatex@gmail.com>
2023-05-23 06:59:21 -0700

    prints all

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives including the first one
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+main(int argc, char *argv[])
+{
+	int index;
+
+	for (index = 0; index < argc; index++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
