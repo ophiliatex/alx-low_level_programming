3d3bb11175b81a9e3b85170df06a470a15f28968
ophiliatex <ophiliatex@gmail.com>
2023-05-23 22:47:12 -0700

    adds

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - adds two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: size
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int a = 0, b = 0, c = 0, d = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[a])
+		a++;
+
+	while (s2[c])
+		c++;
+
+	if (n >= c)
+		d = a + c;
+	else 
+		d = a + n;
+
+	str = malloc(sizeof(char) * d + 1);
+	if (str == NULL)
+		return (NULL);
+
+	c = 0;
+	while (b < d)
+	{
+		if (b <= a)
+			str[b] = s1[b];
+
+		if (b >= a)
+		{
+			str[b] = s2[c];
+			c++;
+		}
+		b++;
+	}
+	str[b] = '\0';
+	return (str);
+}
