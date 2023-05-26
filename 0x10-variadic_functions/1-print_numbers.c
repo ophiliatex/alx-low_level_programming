0bdba0267adf366f05e2e48f2b2168ca938a8d17
ophiliatex <ophiliatex@gmail.com>
2023-05-25 19:54:46 -0700

    print with variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned  int index;
+
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (index < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(nums);
+}
