ea68ca1a9e4303c7789c31a961334c9929fe2f96
ophiliatex <ophiliatex@gmail.com>
2023-05-25 21:42:46 -0700

    variadic

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,9 +21,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		strg = va_arg(strgs, char *);
 		if (strg == NULL)
 			printf("(nil)");
-		else 
+		else
 			printf("%s", strg);
-		if (index < n -1 && separator != NULL)
+		if (index < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 			printf("\n");
