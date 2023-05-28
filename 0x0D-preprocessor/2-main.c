26c7f2a244c58a8d88d656eee696a4b6b105af39
ophiliatex <ophiliatex@gmail.com>
2023-05-27 22:45:59 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,8 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+/**
+ * main - Entry point
+ * Description: A program that prints the name of the file it was compiled from
+ * Return: void
+ */
+
+int main(void)
 {
-    printf("%s\n", __FILE__);
-    return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
