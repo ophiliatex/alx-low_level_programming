8f5dd2dcbc46548dd558929a9670147d903f692b
ophiliatex <ophiliatex@gmail.com>
2023-05-28 14:04:48 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,9 +17,9 @@ struct dog
 
 typedef struct dog dog_t;
 
-void init_dog(struct dog *t, char *name, float age, char *owner);
-void print_dog(struct dog *t);
-dog_t *n_dog(char *name, float age, char *owner);
-void free_dog(dog_t *t);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* struct dog */
