504ad66091e2a0540b268c31b15c7d587c22ed1e
ophiliatex <ophiliatex@gmail.com>
2023-06-06 09:52:45 -0800

    singly links

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2: frees a list
+ * @head: pointer to the head pointer
+ * 
+ * Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+    listint_t *tmp;
+
+    if (head == NULL)
+    return;
+
+    while (*head != NULL)
+{
+    tmp = *head;
+    *head = (*head)->next;
+    free(tmp);
+}
+}
