48a473f00a5e6f7cae8f6ec9fc4c93894df87c2e
ophiliatex <ophiliatex@gmail.com>
2023-08-31 18:28:01 -0800

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,9 @@
 * unsigned int.
 * @b: binary.
 *
-* Return: unsigned int.
+* Return: the converted number, or 0 if
+* there is one or more chars in the string b that is not 0 or 1
+* b is NULL
 */
 unsigned int binary_to_uint(const char *b)
 {
